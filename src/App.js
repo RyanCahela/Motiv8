0b458f37287a43db9d1ab9383eb846cb18b68900
ryancahela <ryancahela@gmail.com>
2019-06-12 22:56:47 -0700

    makes single bg image request

--- src/App.js
+++ src/App.js
@@ -46,23 +46,51 @@ class App extends React.Component {
     });
   }
 
+  getQuoteAssets = () => {
+    console.log(this);
+
+    const getNewBackgroundUrl = () => {
+      fetch('https://api.unsplash.com/photos/random', {
+        headers: {
+          Authorization: 'Client-ID 637ad8107185907a6e559449be25e4c6fb9429f66f500149003592bbf8bf49ce'
+        }
+      })
+      .then(res => {
+        console.log(res);
+        if(res.ok) {
+          return res.json();
+        }
+        throw new Error(res);
+      })
+      .then(resJson => {
+        console.log(this);
+        console.log(resJson);
+        this.setState({
+          backgroundImageUrl: resJson.urls.small
+        });
+      })
+      .catch(e => console.log(e));
+  }
+  getNewBackgroundUrl()
+}
+
   render() {
+    //create components to pass down
     const quoteDisplay = (
       <QuoteDisplay 
       backgroundImageUrl={this.state.backgroundImageUrl} 
       fontFamily={this.state.fontFamily}
       quote={this.state.quotes[0]}/>
     )
-
     const quoteControls = (
-      <QuoteControls/>
+      <QuoteControls />
     )
 
     return (
         <>
         <Menu />
         <Quote quoteDisplay={quoteDisplay} quoteControls={quoteControls}/>
-        <Nav />
+        <Nav getQuoteAssets={this.getQuoteAssets}/>
         </>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Nav() {
+export default function Nav(props) {
   return (
     <div>
       <button>Undo</button>
-      <button>Randomize</button>
+      <button onClick={() => props.getQuoteAssets()}>Randomize</button>
     </div>
   )
 }
