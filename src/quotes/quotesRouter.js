261d281809b4d285ab404dd04f51630aca6413b4
ryancahela <ryancahela@gmail.com>
2019-07-22 22:40:24 -0700

    working deploy

--- src/quotes/quotesRouter.js
+++ src/quotes/quotesRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const QuotesServices = require('./QuotesServices');
-
 const quotesRouter = express.Router();
 
 quotesRouter
@@ -8,15 +7,17 @@ quotesRouter
   .get((req, res, next) => {
 
     const randomNumArray = generateRandomNumArrayWithLength(30);
+    console.log(randomNumArray);
     QuotesServices.getQuotesByIdArray(req.app.get('db'), randomNumArray)
       .then(quotes => {
+        console.log(quotes);
         res.status(200).json(quotes);
       })
       .catch(next);
 
     function generateRandomNumArrayWithLength(length) {
       const ID_RANGE = 5000;
-      return Array.from({length: length}, () => Math.ceil(Math.random() * ID_RANGE));
+      return Array.from({length: length}, () => Math.floor(Math.random() * ID_RANGE));
     }
   })
 
